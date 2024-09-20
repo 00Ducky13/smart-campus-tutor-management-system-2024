adf9f6f47c1d11c378d762859422c44365e1c6b6
00Ducky13 <131754461+00Ducky13@users.noreply.github.com>
2024-09-20 12:49:21 +0200

    redirect to new dashboard

--- src/script.js
+++ src/script.js
@@ -62,7 +62,7 @@ async function onSignIn(e) {
          window.location.href = "tutordashboard.html";
       }
       else{
-          window.location.href = "tutApp/index.html";
+          window.location.href = "../tutApp/index.html";
       }
   }
   else {
