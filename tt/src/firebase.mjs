097d67461411aa78c20d5b53228cc984e3bd6f9f
KaraboWits <160496062+KaraboWits@users.noreply.github.com>
2024-09-20 12:08:02 +0200

    Add files via upload

--- tt/src/firebase.mjs
+++ tt/src/firebase.mjs
@@ -1,22 +1,16 @@
  // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
- import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+ import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
 
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
-
- // Your web app's Firebase configuration
- const firebaseConfig = {
-   apiKey: "AIzaSyDdG4b8bSCU_O9aFzV4DIY3MdOqDr3qWgs",
-   authDomain: "tutoring-app-2e2a8.firebaseapp.com",
-   databaseURL: "https://tutoring-app-2e2a8-default-rtdb.firebaseio.com/",
-   projectId: "tutoring-app-2e2a8",
-   storageBucket: "tutoring-app-2e2a8.appspot.com",
-   messagingSenderId: "311046591355",
-   appId: "1:311046591355:web:0d3ba804d13317c2fb63de"
- };
- 
+  const firebaseConfig = {
+    apiKey: "AIzaSyAWh-Qb5yO6knnHTScmjISsUMt-ofs1RdM",
+    authDomain: "witstutoring-3d624.firebaseapp.com",
+    projectId: "witstutoring-3d624",
+    storageBucket: "witstutoring-3d624.appspot.com",
+    messagingSenderId: "261185278289",
+    appId: "1:261185278289:web:29e063158454a0c3a60370"
+  };
 
 
 
