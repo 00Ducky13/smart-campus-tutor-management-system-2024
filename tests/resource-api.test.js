a5de55f230c49d17be9de89da99e3996c6077bda
00Ducky13 <131754461+00Ducky13@users.noreply.github.com>
2024-10-01 08:17:40 +0000

    Delete tests directory

--- tests/resource-api.test.js
+++
@@ -1,8 +0,0 @@
-//const { getAllTestResources,uploadTestResource } = require("../api/resource-api.js");
-
-describe('Sanity Test', () => { 
-    it('should be true ', () => {
-        const sanityTemp = true;
-        expect(sanityTemp).toBe(true);
-      });
-  });
