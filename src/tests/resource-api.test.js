23d4c3fdd01c0b5de4f04eed72f88a441c952996
00Ducky13 <131754461+00Ducky13@users.noreply.github.com>
2024-10-01 08:16:17 +0000

    resource page to src folder

---
+++ src/tests/resource-api.test.js
@@ -0,0 +1,8 @@
+//const { getAllTestResources,uploadTestResource } = require("../api/resource-api.js");
+
+describe('Sanity Test', () => { 
+    it('should be true ', () => {
+        const sanityTemp = true;
+        expect(sanityTemp).toBe(true);
+      });
+  });
