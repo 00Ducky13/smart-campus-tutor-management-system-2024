4c0496aa6cedecb028683df9dec35ae3deb678a8
00Ducky13 <131754461+00Ducky13@users.noreply.github.com>
2024-10-03 17:41:00 +0000

    Add user management files

---
+++ src/indexScripts.js
@@ -0,0 +1,98 @@
+//import { AuthenticationService } from "./authenticationService.mjs";
+//import { UserRepository } from "./userRepository.mjs";
+//import { firestore, authentication } from "./firebase.mjs";
+import { registerUser,signInUser } from "./api/user-api.js";
+
+
+async function onSignUp(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const username = formData.get("username");
+  const role = formData.get("role");
+
+  await registerUser(email,password,username,role).then(res =>{
+    if (res == 0){
+      console.log("success");
+      if (role === "tutor"){
+        window.location.href = "tutordashboard.html";
+    }
+    else{
+        window.location.href = "studentdashboard.html";
+    }
+  }
+  else{
+    if (res == 1){
+      alert("Email already in use. Sign In instead?");
+    }
+    else if (res == 6){
+      alert("Please use your Wits email.");
+    }
+  }
+  });
+}
+  
+
+async function onSignIn(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+    
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const role = formData.get("role");
+  await signInUser(email,password,role).then(res =>{
+    if (res == 0){
+      console.log("success");
+      if (role === "tutor"){
+        window.location.href = "tutordashboard.html";
+    }
+    else{
+        window.location.href = "studentdashboard.html";
+    } 
+    }
+    else{
+      if (res == 1){
+        alert("An error occured during sign in");
+      }
+      if (res == 2){
+        alert("User does not exist.")
+      }
+    }
+  });
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Content Loaded");
+  const signUpLink = document.getElementById("sign-up");
+  const loginLink = document.getElementById("login");
+  const loginForm = document.getElementById("login-form");
+  const signUpForm = document.getElementById("signup-form");
+
+  if (signUpLink && loginLink && loginForm && signUpForm) {
+    console.log("Links made");
+    signUpLink.addEventListener("click", (event) => {
+      event.preventDefault();
+      loginForm.style.display = "none";
+      signUpForm.style.display = "block";
+    });
+
+    loginLink.addEventListener("click", (event) => {
+      event.preventDefault();
+      loginForm.style.display = "block";
+      signUpForm.style.display = "none";
+    });
+
+    signUpForm.addEventListener("submit", (e) => {onSignUp(e)});
+    loginForm.addEventListener("submit", (e) => {
+        console.log("In Sign In");
+        onSignIn(e);});
+  } else {
+    console.error("One or more elements are missing.");
+  }
+});
+
---
+++ src/user-api.js
@@ -0,0 +1,74 @@
+import {
+    setDoc,
+    doc,
+    getDoc,
+    deleteDoc,
+    addDoc
+  } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+import {authentication,firestore} from "../firebase/firebase.js";
+import { createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+
+async function registerUser(email, password, username, role){
+    let inHouseErrorCode = 0
+    if (email.indexOf("wits.ac.za") === -1){
+        inHouseErrorCode = 6;
+    }
+    else{
+    await createUserWithEmailAndPassword(authentication, email, password)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      // ...
+      const userRef = setDoc(doc(firestore, "test-users", user.uid), {
+        role: role,
+        username: username
+      },{merge: true});
+    })
+    .catch((error) => {
+        
+        switch (error.code) {
+            case 'auth/email-already-in-use':
+              inHouseErrorCode = 1
+              break;
+            case 'auth/invalid-email':
+              inHouseErrorCode = 2
+              break;
+            case 'auth/operation-not-allowed':
+              inHouseErrorCode = 3
+              break;
+            case 'auth/weak-password':
+              inHouseErrorCode = 4
+              break;
+            default:
+              console.log(error.message);
+              inHouseErrorCode = 5
+              break;
+          }
+
+    });
+}
+    return inHouseErrorCode
+}
+
+async function signInUser(email, password, role){
+    let inHouseErrorCode = 0
+    await signInWithEmailAndPassword(authentication, email, password)
+    .then(async (userCredential) => {
+        const user = userCredential.user;
+        const docRef = doc(firestore, "test-users", user.uid);
+        await getDoc(docRef).then(docSnap => {
+            if (docSnap.data().role !== role){
+                inHouseErrorCode = 2
+                signOut(authentication);
+            }
+        });      
+    })
+    .catch((error) => {
+        
+        inHouseErrorCode = 1
+
+    });   
+    return inHouseErrorCode
+}
+
+export {registerUser,signInUser}
