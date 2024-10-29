255ab63f3def717ef3bb0a8c4d9019d33f8f6e49
Danielle Smal <2312053@students.wits.ac.za>
2024-10-29 18:02:37 +0200

    get all tutor and show on student dash

--- src/studentdashscript.js
+++ src/studentdashscript.js
@@ -1,3 +1,14 @@
+import {getTutorFromID,updateUserWithID,getAllTutors} from "./tutorapi.js";
+
+let master_tutor_list = [];
+let fullFilterList = [];
+async function populateTutorList(){
+    let results = await getAllTutors();
+    for(let i=0;i<results.length;i++){
+        let tempObj = results[i];
+        master_tutor_list.push(tempObj);
+    }
+}
 // Handle star rating for tutors
 document.querySelectorAll('.star-rating input').forEach(star => {
     star.addEventListener('change', function() {
@@ -42,7 +53,49 @@ window.addEventListener('DOMContentLoaded', function() {
     if (scheduledTutor && scheduledDate && scheduledTime) {
         alert(`Upcoming session with ${scheduledTutor} on ${scheduledDate} at ${scheduledTime}`);
     }
+
+    console.log("Content Loaded");
+    populateTutorList().then(res =>{
+        fullFilterList = [].concat(master_tutor_list);
+        console.log(master_tutor_list);
+        removeTutorCards();
+        drawTutorCards(master_tutor_list);
+    }); 
 });
+
+
+function drawTutorCards(tutor_list){
+    const tutorGrid = document.getElementById("tutorGrid");
+    for (let i=0;i<master_tutor_list.length;i++){
+        //console.log("Item found");    
+        let tutorCard = document.createElement("a");
+        tutorCard.setAttribute("class","tutor-item");
+        tutorCard.setAttribute("href",tutor_list[i].link);
+        tutorCard.setAttribute("id","tutorCard-"+i.toString())
+        tutorCard.setAttribute("display","");
+        let tutorName = document.createElement("p");
+        tutorName.innerHTML = "<strong>" + tutor_list[i].fullname + "</strong>";
+        tutorCard.appendChild(tutorName);
+        /* let tutorSubject = document.createElement("p");
+        tutorSubject.innerHTML = "<strong>Subject:</strong> " + tutor_list[i].subject;
+        tutorCard.appendChild(tutorSubject);
+        let tutorCourse = document.createElement("p");
+        tutorCourse.innerHTML = "<strong>Course:</strong> " + tutor_list[i].course;
+        tutorCard.appendChild(tutorCourse);
+        let tutorOwner = document.createElement("p");
+        tutorOwner.innerHTML = "<strong>Uploader:</strong> " + tutor_list[i].owner; */
+       // tutorCard.appendChild(tutorOwner);
+        tutorGrid.appendChild(tutorCard);
+    }    
+}
+function removeTutorCards(){
+    for (let i=0;i<master_tutor_list.length;i++){
+        let tempCard = document.getElementById("tutorCard-"+i.toString());
+        if (tempCard !== null){
+            tempCard.remove();
+        }
+    }
+}
 
 // Smooth scrolling for navigation links
 document.querySelectorAll('.navbar a').forEach(link => {
